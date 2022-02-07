e76c4a362043ac617cf69efa594bf043f2ad03a4
krskovape <krskovape@natur.cuni.cz>
2022-02-07 12:59:22 +0100

    Setřídění posloupnosti a úprava cyklu pro duplicitní prvky

--- zk_1.py
+++ zk_1.py
@@ -34,26 +34,53 @@ def WriteToFile(file_name, text):
         print(f"Unexpected error opening {file_name}: {e}")
         quit()
     
-#Loading sequence from input file and initialization of variables
+#Function for converting string elements into integers
+def StrToInt(list):
+    sequence = []
+    index = -1
+    for x in list:
+        index += 1
+        try:
+            y = int(x)
+            sequence.append(y)
+        except ValueError:
+            print(f"Element in position {index} in {list} is not a number and the program will skip this element")
+            continue
+    return sequence
+
+#Function for ascending sorting of list values
+def SortList(input_list):
+    for i in range(len(input_list)):
+        min = i
+        for j in range(i+1, len(input_list)):
+            if input_list[min] > input_list[j]:
+                min = j
+        input_list[i], input_list[min] = input_list[min], input_list[i]
+    return input_list
+
+#Loading sequence from file, converting elements into integers and sorting it
 sequence = SequenceFromFile("input.txt")
-final_sequence = []
-duplicates = {}
-index = -1
+int_sequence = StrToInt(sequence)
+l = SortList(int_sequence)
 
 #Loop for recognition of duplicate elements, storing their quantity and saving unique elements into new sequence and output file
-for object in sequence:
-    index += 1
-    try:
-        element = int(object)
-    except ValueError:
-        print(f"Element in position {index} is not a number and the program will skip this element")
-        continue
-    
-    if element in final_sequence:
-        duplicates[element] = duplicates.setdefault(element, 0) + 1
+i = 0
+final = []
+duplicates = {}
+while i < (len(l)-1):
+    j = i+1
+    if l[i] != l[j] and l[i] not in final:
+        final.append(l[i])
+        WriteToFile("output.txt", f"{l[i]} ")
     else:
-        final_sequence.append(element) 
-        WriteToFile("output.txt", f"{element} ")
+        duplicates[l[i]] = duplicates.setdefault(l[j], 0) + 1
+    i+=1
+
+#Check if the last element is in final list, if not append it
+max = len(l)-1
+if l[max] not in final:
+    final.append(l[max])
+    WriteToFile("output.txt", f"{l[max]} ")
 
 #Loop for writing duplicate elements and their quantity into output file
 WriteToFile("output.txt", f"\n\nThese duplicate elements ({sum(duplicates.values())} in total) were deleted from original sequence:\n")
